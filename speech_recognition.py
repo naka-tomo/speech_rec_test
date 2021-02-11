3c5ea7a458ebf78bba4bc3e8807482b483a2af33
naka_t <20672947+naka-tomo@users.noreply.github.com>
2021-02-11 21:02:05 +0900

    音声合成機能追加

--- speech_recognition.py
+++ speech_recognition.py
@@ -1,4 +1,5 @@
 from websocket_server import WebsocketServer
+import threading
 
 def message_received(client, server, message):
     # 日本語の文字コードがおかしいので修正
@@ -11,12 +12,21 @@ def new_client(client, server):
 def client_left(client, server):
     print("クライアント切断")
 
+def say_thread( server ):
+    while 1:
+        txt = input("発話内容入力 ->")
+        server.send_message_to_all( txt )
 
 def main():
     server = WebsocketServer(60000, host="127.0.0.1")
     server.set_fn_new_client(new_client)
     server.set_fn_client_left(client_left)
     server.set_fn_message_received(message_received) 
+
+    th = threading.Thread(target=say_thread , args=[server])
+    th.setDaemon(True)
+    th.start()
+
     server.run_forever()
 
 if __name__=="__main__":
